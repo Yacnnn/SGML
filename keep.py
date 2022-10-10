f598abc17aa736a8b26b6c18d851b442e00643d3
Yacnnn <73817884+Yacnnn@users.noreply.github.com>
2022-10-10 18:50:13 +0200

    Delete keep.py

--- keep.py
+++
@@ -1,7 +0,0 @@
-                    XX = tf.einsum('ij,ij->i', output[i], output[i])[:, np.newaxis]
-                    YY = tf.einsum('ij,ij->i', output[j], output[j])[np.newaxis, :]
-                    distances = tf.tensordot(output[i], tf.transpose(output[j]), [[1],[0]])
-                    distances *= -2
-                    distances += XX
-                    distances += YY
-                    distances =  tf.nn.relu(distances)
